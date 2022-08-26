e4d5c9739dee591c4e59ec9118ec00234f17a4ae
Abdessattar Sassi <457645+abdes@users.noreply.github.com>
2022-08-26 22:58:05 +0400

    refactor: remove no longer used git clone method

--- cmake/GetCryptoppSources.cmake
+++ cmake/GetCryptoppSources.cmake
@@ -4,32 +4,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # ===-----------------------------------------------------------------------===#
 
-function(use_gitclone)
-  if(${CRYPTOPP_USE_MASTER_BRANCH})
-    set(source_location GIT_BRANCH "master")
-  else()
-    set(source_location
-        GIT_TAG
-        "CRYPTOPP_${META_VERSION_MAJOR}_${META_VERSION_MINOR}_${META_VERSION_PATCH}"
-    )
-  endif()
-  file(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/external)
-  include(GitClone)
-  git_clone(
-    PROJECT_NAME
-    ext_cryptopp
-    GIT_URL
-    ${META_GITHUB_REPO}
-    ${source_location}
-    DIRECTORY
-    ${CMAKE_CURRENT_BINARY_DIR}/external
-    QUIET)
-  set(CRYPTOPP_PROJECT_DIR
-      ${ext_cryptopp_SOURCE_DIR}
-      PARENT_SCOPE)
-endfunction()
-
-function(use_fetch_content)
+function(get_cryptopp_sources)
   find_package(Git QUIET)
   if(GIT_FOUND)
     if(${CRYPTOPP_USE_MASTER_BRANCH})
@@ -52,8 +27,7 @@ function(use_fetch_content)
   include(FetchContent)
   set(FETCHCONTENT_BASE_DIR ${CMAKE_CURRENT_BINARY_DIR}/external)
   file(MAKE_DIRECTORY ${FETCHCONTENT_BASE_DIR})
-  FetchContent_Declare(ext_cryptopp ${source_location})
-  FetchContent_Populate(ext_cryptopp)
+  FetchContent_Populate(ext_cryptopp ${source_location})
   FetchContent_GetProperties(ext_cryptopp SOURCE_DIR)
   set(CRYPTOPP_PROJECT_DIR
       ${ext_cryptopp_SOURCE_DIR}
