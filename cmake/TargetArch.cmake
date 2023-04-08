f5f27bf7ed91ac65d50486304ec8401957753921
Vollstrecker <werner@vollstreckernet.de>
2023-04-08 18:35:32 +0200

    fix: detection and flags for mingw32

--- cmake/TargetArch.cmake
+++ cmake/TargetArch.cmake
@@ -17,6 +17,8 @@ set(archdetect_c_code
 #    error cmake_ARCH arm
 #  endif
 
+#elif defined(__MINGW32__)
+    #error cmake_ARCH mingw32
 #elif defined(__i386) || defined(__i386__) || defined(_M_IX86)
     #error cmake_ARCH i386
 #elif defined(__x86_64) || defined(__x86_64__) || defined(__amd64) || defined(_M_X64)
