ef62e4a7b9f956852890988e87385709b1b1f28a
Vollstrecker <werner@vollstreckernet.de>
2023-04-21 14:32:01 +0200

    chore: moved project upwards

--- cmake/GetCryptoppSources.cmake
+++ cmake/GetCryptoppSources.cmake
@@ -10,7 +10,7 @@ macro(use_gitclone)
   else()
     set(source_location
         GIT_TAG
-        "CRYPTOPP_${META_VERSION_MAJOR}_${META_VERSION_MINOR}_${META_VERSION_PATCH}"
+        "CRYPTOPP_${version_underscore}"
     )
   endif()
   file(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/_deps)
@@ -19,7 +19,7 @@ macro(use_gitclone)
     PROJECT_NAME
     "${CRYPTOPP_INCLUDE_PREFIX}"
     GIT_URL
-    ${META_GITHUB_REPO}
+    ${cryptopp-cmake_HOMEPAGE_URL}
     ${source_location}
     DIRECTORY
     ${CMAKE_CURRENT_BINARY_DIR}/_deps
@@ -39,7 +39,7 @@ macro(use_url_download)
     file(MAKE_DIRECTORY ${FETCHCONTENT_BASE_DIR})
     FetchContent_Populate(
       cryptopp
-      URL "${META_GITHUB_REPO}/releases/download/CRYPTOPP_${META_VERSION_MAJOR}_${META_VERSION_MINOR}_${META_VERSION_PATCH}/cryptopp${META_VERSION_MAJOR}${META_VERSION_MINOR}${META_VERSION_PATCH}.zip"
+      URL "${cryptopp-cmake_HOMEPAGE_URL}/releases/download/CRYPTOPP_${version_underscore}/cryptopp${cryptopp-cmake_VERSION_MAJOR}${cryptopp-cmake_VERSION_MINOR}${cryptopp-cmake_VERSION_PATCH}.zip"
       QUIET
       SOURCE_DIR
         ${FETCHCONTENT_BASE_DIR}/cryptopp-src/${CRYPTOPP_INCLUDE_PREFIX}
@@ -55,6 +55,7 @@ endmacro()
 function(get_cryptopp_sources)
   # If we have git on the system, prefer the basic git workflow, which is more
   # predictable and straightforward than the FetchContent.
+  set (version_underscore "${cryptopp-cmake_VERSION_MAJOR}_${cryptopp-cmake_VERSION_MINOR}_${cryptopp-cmake_VERSION_PATCH}")
   if(GIT_FOUND)
     use_gitclone()
   else()
