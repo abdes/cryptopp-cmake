4b0d281a188190b2d4b50b5049e3b75186ed22b1
Vollstrecker <werner@vollstreckernet.de>
2023-05-24 18:01:51 +0200

    chore: no need for this to be a function

--- cmake/GetCryptoppSources.cmake
+++ cmake/GetCryptoppSources.cmake
@@ -4,38 +4,34 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # ===-----------------------------------------------------------------------===#
 
-function(get_cryptopp_sources)
-  include(FetchContent)
-  set (version_underscore "${cryptopp-cmake_VERSION_MAJOR}_${cryptopp-cmake_VERSION_MINOR}_${cryptopp-cmake_VERSION_PATCH}")
-  if(GIT_FOUND)
-    if(${CRYPTOPP_USE_MASTER_BRANCH})
-      set(source_location "master")
-    else()
-      set(source_location
-          "CRYPTOPP_${version_underscore}"
-      )
-    endif()
+include(FetchContent)
+set (version_underscore "${cryptopp-cmake_VERSION_MAJOR}_${cryptopp-cmake_VERSION_MINOR}_${cryptopp-cmake_VERSION_PATCH}")
+if(GIT_FOUND)
+  if(${CRYPTOPP_USE_MASTER_BRANCH})
+    set(source_location "master")
+  else()
+    set(source_location
+        "CRYPTOPP_${version_underscore}"
+    )
+  endif()
+  FetchContent_Declare(
+    cryptopp
+    GIT_REPOSITORY ${cryptopp-cmake_HOMEPAGE_URL}
+    GIT_TAG ${source_location}
+    QUIET
+    SOURCE_DIR ${CRYPTOPP_INCLUDE_PREFIX})
+else()
+  message(STATUS "Downloading crypto++ from URL...")
+  cmake_policy(SET CMP0135 NEW)
+  if(NOT ${CRYPTOPP_USE_MASTER_BRANCH})
     FetchContent_Declare(
       cryptopp
-      GIT_REPOSITORY ${cryptopp-cmake_HOMEPAGE_URL}
-      GIT_TAG ${source_location}
-      QUIET
-      SOURCE_DIR ${CRYPTOPP_INCLUDE_PREFIX}
-  )
-  else()
-    message(STATUS "Downloading crypto++ from URL...")
-    cmake_policy(SET CMP0135 NEW)
-    if(NOT ${CRYPTOPP_USE_MASTER_BRANCH})
-      FetchContent_Declare(
-        cryptopp
-          URL "${cryptopp-cmake_HOMEPAGE_URL}/releases/download/CRYPTOPP_${version_underscore}/cryptopp${cryptopp-cmake_VERSION_MAJOR}${cryptopp-cmake_VERSION_MINOR}${cryptopp-cmake_VERSION_PATCH}.zip"
-          QUIET
-          SOURCE_DIR ${CRYPTOPP_INCLUDE_PREFIX}
-      )
-    endif()
+        URL "${cryptopp-cmake_HOMEPAGE_URL}/releases/download/CRYPTOPP_${version_underscore}/cryptopp${cryptopp-cmake_VERSION_MAJOR}${cryptopp-cmake_VERSION_MINOR}${cryptopp-cmake_VERSION_PATCH}.zip"
+        QUIET
+        SOURCE_DIR ${CRYPTOPP_INCLUDE_PREFIX}
+    )
   endif()
-  FetchContent_Populate(cryptopp)
-  set(CRYPTOPP_PROJECT_DIR
-      ${cryptopp_SOURCE_DIR}
-      PARENT_SCOPE)
-endfunction()
+endif()
+FetchContent_Populate(cryptopp)
+set(CRYPTOPP_PROJECT_DIR
+    ${cryptopp_SOURCE_DIR})
