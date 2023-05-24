d5b4357587338522021e2be63528770f1da1c308
Vollstrecker <werner@vollstreckernet.de>
2023-05-24 16:58:14 +0200

    chore: move from gitclone to fetchcontent

--- cmake/GetCryptoppSources.cmake
+++ cmake/GetCryptoppSources.cmake
@@ -6,25 +6,20 @@
 
 macro(use_gitclone)
   if(${CRYPTOPP_USE_MASTER_BRANCH})
-    set(source_location GIT_BRANCH "master")
+    set(source_location "master")
   else()
     set(source_location
-        GIT_TAG
         "CRYPTOPP_${version_underscore}"
     )
   endif()
-  file(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/_deps)
-  include(GitClone)
-  git_clone(
-    PROJECT_NAME
-    "${CRYPTOPP_INCLUDE_PREFIX}"
-    GIT_URL
-    ${cryptopp-cmake_HOMEPAGE_URL}
-    ${source_location}
-    DIRECTORY
-    ${CMAKE_CURRENT_BINARY_DIR}/_deps
-    SOURCE_DIR_VARIABLE
-    cryptopp_SOURCE_DIR)
+  FetchContent_Declare(
+    cryptopp
+    GIT_REPOSITORY ${cryptopp-cmake_HOMEPAGE_URL}
+    GIT_TAG ${source_location}
+    QUIET
+    SOURCE_DIR ${CRYPTOPP_INCLUDE_PREFIX}
+  )
+  FetchContent_Populate(cryptopp)
   set(CRYPTOPP_PROJECT_DIR
       ${cryptopp_SOURCE_DIR}
       PARENT_SCOPE)
@@ -33,7 +28,6 @@ endmacro()
 macro(use_url_download)
   cmake_policy(SET CMP0135 NEW)
   if(NOT ${CRYPTOPP_USE_MASTER_BRANCH})
-    include(FetchContent)
     FetchContent_Declare(
       cryptopp
       URL "${cryptopp-cmake_HOMEPAGE_URL}/releases/download/CRYPTOPP_${version_underscore}/cryptopp${cryptopp-cmake_VERSION_MAJOR}${cryptopp-cmake_VERSION_MINOR}${cryptopp-cmake_VERSION_PATCH}.zip"
@@ -50,6 +44,7 @@ endmacro()
 function(get_cryptopp_sources)
   # If we have git on the system, prefer the basic git workflow, which is more
   # predictable and straightforward than the FetchContent.
+  include(FetchContent)
   set (version_underscore "${cryptopp-cmake_VERSION_MAJOR}_${cryptopp-cmake_VERSION_MINOR}_${cryptopp-cmake_VERSION_PATCH}")
   if(GIT_FOUND)
     use_gitclone()
